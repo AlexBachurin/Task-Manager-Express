e803c582c454ff61c8869043441eb89594e74507
Aleksandr <alexbachurin91@gmail.com>
2023-03-17 15:46:48 +0300

    get single task + some error handling

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -1,17 +1,40 @@
 const Task = require("../models/Task");
 
 //get All tasks
-const getAllTasks = (req, res) => {
-  res.send("get All Tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 //get Single task
-const getSingleTask = (req, res) => {
-  res.send("get single task");
+const getSingleTask = async (req, res) => {
+  try {
+    // get id from request params
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    // if id is wrong we get task as null, so check for task
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id of ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+  // res.json({ id });
 };
 //POST to create task
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  // handle error when creating task
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    // at this point just send back response as server error if there is an error
+    res.status(500).json({ error });
+  }
 };
 // PATCH to edit task
 const editTask = (req, res) => {
